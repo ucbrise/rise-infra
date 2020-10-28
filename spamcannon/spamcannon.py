2cf6838521989ccb3bd83e6d7f6b5ce6fdc3a8e3
shane knapp <incomplete@gmail.com>
2020-10-27 17:44:33 -0700

    ready for testing

--- spamcannon/spamcannon.py
+++ spamcannon/spamcannon.py
@@ -17,10 +17,22 @@ import os
 import smtplib
 import socket
 import sys
+import time
 
 locale.setlocale(locale.LC_ALL, '') # for comma formatting
 
-PROJECTS = ['test1', 'foo', 'bar']
+PROJECT_PREAMBLE = collections.defaultdict(dict)
+PROJECTS = ['MC2', 'Ray', 'ERDOS']
+PROJECT_PREAMBLE['MC2'] = '''
+SOME TEXT
+'''
+PROJECT_PREAMBLE['Ray'] = '''
+zomg text
+'''
+
+PROJECT_PREAMBLE['ERDOS'] = '''
+even MOAR text
+'''
 
 TODAY = datetime.date.today()
 DAY = TODAY.strftime('%d')
@@ -32,7 +44,7 @@ MAIL_SERVER = "watson"
 EMAIL_FROM_ADDR = "risecamp@cs.berkeley.edu"
 EMAIL_REPLY_ADDR = "sknapp+spamcannon@berkeley.edu"
 
-EMAIL_SUBJECT = "IMPORTANT: RISECamp Tutorial Links"
+EMAIL_SUBJECT = "IMPORTANT: RISECamp 2020 Tutorial Links"
 
 EMAIL_PREAMBLE = """
 Greetings from RISELab!  Here are the details for your online tutorials:
@@ -40,25 +52,48 @@ Greetings from RISELab!  Here are the details for your online tutorials:
 
 """
 
-def send_email(links, destination):
+def generate_spam(attendee_hash, send_email=False, delay=1):
     """
     for each hash entry in destination, send a corresponding set of project links
     """
+    burst = 60 # 60 emails before pause
+    pause = 30 # pause for 30
+    count = 0
 
-def parse_links():
-    """
-    read in links for each file, store in a dict and return
-    """
-    link_hash = collections.defaultdict(dict)
+    for email in attendee_hash:
+        message_body = EMAIL_PREAMBLE + \
+                       '--------------------\n\n'
 
-    for file in PROJECTS:
-        file = file + '-links.txt'
-        with open(file, 'r') as f:
-            for line in f:
-                link = line[:1]
-                link_hash[project] = link
+        for project in PROJECTS:
+            message_body += PROJECT_PREAMBLE[project] + \
+                            attendee_hash[email][project.lower() +'-ip'] + '\n\n'
 
-    return link_hash
+        message_body += """
+Please use Slack for technical support
+        """
+
+        print(message_body)
+
+        if send_email:
+            if count == burst:
+                count = 0
+                time.sleep(pause)
+
+            message_body = MIMEText(message_body)
+
+            msg = MIMEMultipart()
+            msg['Subject'] = EMAIL_SUBJECT
+            msg['From'] = EMAIL_FROM_ADDR
+            msg['To'] = email
+            msg['Reply-To'] = EMAIL_REPLY_ADDR
+            msg.attach(message_body)
+
+            s = smtplib.SMTP(MAIL_SERVER)
+            s.sendmail(EMAIL_FROM_ADDR,
+                       [EMAIL_TO_ADDR],
+                       msg.as_string())
+            time.sleep(delay)
+            count = count + 1
 
 def parse_attendees(input_data):
     """
@@ -70,8 +105,19 @@ def parse_attendees(input_data):
 
     attendee_data = csv.reader(attendee_raw.split('\n'))
 
-    for line in attendee_data:
-        
+    for row in attendee_data:
+        if row:
+            print(row)
+            email = row[2]
+            attendee_hash[email]['fname'] = row[0]
+            attendee_hash[email]['lname'] = row[1]
+            attendee_hash[email]['mc2-ip'] = row[4]
+            attendee_hash[email]['ray-ip'] = row[5]
+            attendee_hash[email]['erdos-ip'] = row[6]
+
+    return attendee_hash
+
+
 def parse_args():
     """
     parse CLI args
@@ -80,20 +126,34 @@ def parse_args():
     parser = argparse.ArgumentParser(description=desc)
 
     parser.add_argument("-i",
-                        "--input-data",
+                        "--input",
                         help="""csv input data of attendees""",
                         type=str,
                         metavar="INPUT_DATA")
     parser.add_argument("-d",
-                        "--delay"
-                        """delay in seconds between deliveries""",
+                        "--delay",
+                        help="""delay in seconds between deliveries""",
                         type=float,
                         metavar="DELAY")
+    parser.add_argument("-e",
+                        "--email",
+                        help="""actually send the emails""",
+                        action="store_true")
+
+    args = parser.parse_args()
+    return args
 
 
 def main():
     args = parse_args()
+    print(args)
+    if not args.input:
+        sys.exit(-1)
 
+    print(args.email)
+
+    attendees = parse_attendees(args.input)
+    generate_spam(attendees)
 
 if __name__ == "__main__":
     main()
